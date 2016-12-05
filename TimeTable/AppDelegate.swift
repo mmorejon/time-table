22fc89b5775c807cf03d7e0b7d709539bfeed609
Manuel Morejon <manuel.morejon.85@gmail.com>
2016-12-05 12:43:15 -0500

    Remove BuddyBuild framework

--- TimeTable/AppDelegate.swift
+++ TimeTable/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        BuddyBuildSDK.setup()
+//        BuddyBuildSDK.setup()
         
         // Override point for customization after application launch.
         return true
